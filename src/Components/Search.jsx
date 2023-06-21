6547920e28b17f49e165eb20a735a3cdeaed50bc
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2023-06-21 15:54:41 +0530

    Add files via upload

---
+++ src/Components/Search.jsx
@@ -0,0 +1,44 @@
+import { useState, useContext } from "react";
+import { Input, Button } from "@chakra-ui/react";
+import getData from "./Service";
+import AuthContext from "./Context";
+
+const Search = () => {
+  const [input, setInput] = useState("");
+
+  const { addData } = useContext(AuthContext);
+
+  const weatherInfo = () => {
+    getData(input, addData);
+  };
+
+  return (
+    <div style={{ margin: "20px auto" }}>
+      <Input
+        placeholder="Enter City"
+        variant="outline"
+        w="20%"
+        m="10px"
+        size="lg"
+        required
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+
+      <Button
+        colorScheme="teal"
+        variant="solid"
+        m="10px"
+        size="lg"
+        marginTop="1px"
+        // onClick={weatherInfo}
+        onClick={weatherInfo}
+      >
+        Search
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/Components/Service.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+
+const getData = (input, addData) => {
+    if (input === '') {
+        alert("Enter City Name First")
+    } else {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=82f3961c55a872515c85d287083bbba1`)
+            .then((res) => {
+                console.log(res.data)
+                addData(res.data)
+            })
+            .catch(() => {
+                alert('Failed to Load')
+            })
+
+    }
+}
+
+export default getData
